ec783a2dbc013e8008edef87676ae228de9b995b
mtakano <mktkn.mktkn@gmail.com>
2026-05-29 05:14:12 +0900

    あああ

--- Test00/src/Test2.java
+++ Test00/src/Test2.java
@@ -3,7 +3,7 @@ public class Test2 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		System.out.println("test222aFあ");
+		System.out.println("test222aFああ");
 	}
 
 }
--- Test00/src/test00/Test04.java
+++ Test00/src/test00/Test04.java
@@ -4,7 +4,7 @@ public class Test04 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		System.out.println("ts");
+		System.out.println("tsああ");
 	}
 
 }
